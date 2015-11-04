ba5c9687da9d4784765366111e7657e61920b1ed
Giancarlo <guerragiancarlo8@gmail.com>
2015-11-04 14:56:23 +0100

    first method tests success. finished implementing letter,number,symbol check

---
+++ models/password.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require 'pry'
+
+class PasswordChecker
+	def initialize
+
+
+	end
+
+	def check_password(email,password)
+
+		#is_bigger_than_7 password
+	end
+
+	def is_bigger_than_7? string
+		if string.size > 7
+			true
+		else
+			false
+		end
+
+	end
+
+	def letter_number_symbol? string
+
+		check_letters = /[A-Za-z]+/.match(string)
+
+		check_numbers = /[0-9]+/.match(string)
+
+		check_symbols = /[@#%&*]+/.match(string)
+
+
+		if check_letters && check_numbers && check_symbols
+			true
+		else
+			false
+		end
+	end
+end
+
+
